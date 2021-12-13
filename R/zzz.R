a14ee7cba0f5e9ec2c1047fbcdb4412a6e63d300
csaybar <csaybar@gmail.com>
2021-12-13 15:19:32 +0100

    Opps :( critical issue

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,6 @@
 .onLoad <- function(libname, pkgname) {
   # Load Python package available in inst/ folder.
   ee_extra_location <- sprintf("%s/ee_extra", system.file(package = "rgeeExtra"))
-  EEextra_PYTHON_PACKAGE <<- reticulate::import_from_path("ee_extra", ee_extra_location)
+  EEextra_PYTHON_PACKAGE <<- reticulate::import_from_path("ee_extra", ee_extra_location, delay_load = TRUE)
 }
 
